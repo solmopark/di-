8f0fdacdee3085aab5ef6dc8c43c880d9e632334
solmo <94097481+solmopark@users.noreply.github.com>
2022-02-27 22:37:54 +0900

    Add files via upload

---
+++ docs/originer.js
@@ -0,0 +1,113 @@
+// 스와이퍼
+// 추천작
+const swiper1 = new Swiper(" .swiper-box-1 > .swiper", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  slidesPerGroup: 5,
+  touchRatio: 0,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-box-1 .swiper-button-next",
+    prevEl: ".swiper-box-1 .swiper-button-prev"
+  }
+});
+
+// 시리즈
+const swiper2 = new Swiper(" .swiper-box-2 > .swiper", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  slidesPerGroup: 5,
+  touchRatio: 0,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-box-2 .swiper-button-next",
+    prevEl: ".swiper-box-2 .swiper-button-prev"
+  }
+});
+
+//스와이퍼 외
+const swiper0 = new Swiper(" .swiper", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  slidesPerGroup: 5,
+  touchRatio: 0
+});
+
+// 포스터 이미지 전환
+$(document).ready(function () {
+  $(".section02 > .btn > .btn-2").click(function () {
+    $(".section03").hide();
+
+    $(".section03_ver2").show();
+
+    $(".section02 > .text").addClass("active");
+
+    $(this).addClass("active");
+
+    $(".section02 > .btn > .btn-1").removeClass("active");
+  });
+
+  $(".section02 > .btn > .btn-1").click(function () {
+    $(".section03_ver2").hide();
+
+    $(".section03").show();
+
+    $(".section02 > .text").removeClass("active");
+
+    $(this).addClass("active");
+
+    $(".section02 > .btn > .btn-2").removeClass("active");
+  });
+});
+
+$(document).ready(function () {
+  $(".section02 > .btn > .btn-2").click(function () {
+    $(".section03").hide();
+
+    $(".section03_ver2").show();
+
+    $(".section02 > .text").addClass("active");
+
+    $(this).addClass("active");
+
+    $(".section02 > .btn > .btn-1").removeClass("active");
+  });
+
+  $(".section02 > .btn > .btn-1").click(function () {
+    $(".section03_ver2").hide();
+
+    $(".section03").show();
+
+    $(".section02 > .text").removeClass("active");
+
+    $(this).addClass("active");
+
+    $(".section02 > .btn > .btn-2").removeClass("active");
+  });
+});
+
+// up 버튼
+$(document).ready(function () {
+  $(".up-btn").click(function () {
+    $("html , body").animate(
+      {
+        scrollTop: 0
+      },
+      700
+    );
+  });
+});
+
+$(window).scroll(function () {
+  var test = $(this).scrollTop();
+
+  console.log(test);
+
+  if (test >= 1000) {
+    $(".up-btn").addClass("active");
+  } else if (test <= 1000) {
+    $(".up-btn").removeClass("active");
+  }
+});
